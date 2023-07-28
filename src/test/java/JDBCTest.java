82d08466f4cea7a79e479853f166c07b83cd9717
endrmseha <endrmseha@naver.com>
2023-07-28 20:14:47 +0900

    init commit

---
+++ src/test/java/JDBCTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import java.sql.DriverManager;
+
+public class JDBCTest {
+
+
+    @Test
+    void jdbcTest() {
+        DriverManager driverManager;
+    }
+}
